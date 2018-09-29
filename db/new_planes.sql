bd2f836cae5fda983168f2a63f89d8fc9fa1e909
joshparkerj <superfakek@gmail.com>
2018-09-29 11:13:56 -0600

    complete mini code

--- db/new_planes.sql
+++ db/new_planes.sql
@@ -1,5 +1,5 @@
-INSERT INTO airplanes ( plane_type, passenger_count) VALUES ('747', 200);
-INSERT INTO airplanes ( plane_type, passenger_count) VALUES ('717', 10);
-INSERT INTO airplanes ( plane_type, passenger_count) VALUES ('727', 20);
-INSERT INTO airplanes ( plane_type, passenger_count) VALUES ('737', 30);
-INSERT INTO airplanes ( plane_type, passenger_count) VALUES ('757', 500);
+INSERT INTO airplanes ( plane_type, passenger_count) VALUES ('cessna', 2);
+INSERT INTO airplanes ( plane_type, passenger_count) VALUES ('tesla', 1);
+INSERT INTO airplanes ( plane_type, passenger_count) VALUES ('boeing', 9000);
+INSERT INTO airplanes ( plane_type, passenger_count) VALUES ('lockheed martin', 0);
+INSERT INTO airplanes ( plane_type, passenger_count) VALUES ('l3', 3);
--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const massive = require('massive');
+const dotenv = require('dotenv').config();
+const controller = require('./controller');
 
+massive(process.env.CONNECTION_STRING)
+  .then(dbInstance => {
+    app.set('db', dbInstance);
+    //dbInstance.new_planes();
+    //return dbInstance.get_planes();
+  })
+  /*
+  .then(planes => {
+    console.log(planes);
+  })
+  */
+  .catch(err => {
+    console.error(err);
+  })
 const app = express();
 app.use( bodyParser.json() );
 app.use( cors() );
 
+app.get('/api/planes', controller.getPlanes)
+
 const port = process.env.PORT || 3000
 app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
 
+//console.log(process.env);
